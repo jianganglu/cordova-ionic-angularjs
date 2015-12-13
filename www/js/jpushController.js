bd9db33d80aea71e5903b00680ad7865459dea14
jianganglu <1055375563@qq.com>
2015-12-13 22:22:15 +0800

    添加推送通知。

---
+++ www/js/jpushController.js
@@ -0,0 +1,22 @@
+﻿(function() {
+  'use strict';
+
+  angular.module('starter.controllers')
+    .controller('jpushCtrl', jpushCtrl);
+
+  function jpushCtrl() {
+    var vm = this;
+    vm.push = push;
+    vm.isChecked = true;
+
+    function push() {
+      if(window.plugins && window.plugins.jPushPlugin) {
+        if(vm.isChecked) {
+          window.plugins.jPushPlugin.resumePush();
+        }else {
+          window.plugins.jPushPlugin.stopPush();
+        }
+      }
+    }
+  }
+})();
